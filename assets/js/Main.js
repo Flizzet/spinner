fd661ac0cad26416a9a7e071781e08bdf6138634
Flizzet <pedro@flizzet.com>
2018-11-23 05:05:12 -0800

    Implemented PIXI

---
+++ assets/js/Main.js
@@ -0,0 +1,9 @@
+// Creates application
+const app = new PIXI.Application({
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
+    antialias: false,
+    transparent: false,
+    resolution: 1,
+    roundPixels: true
+});
